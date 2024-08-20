4613856c7357d1a46b82f07fd8e141bd5fcfd713
LvYueMing <350087689@qq.com>
2024-08-20 17:38:46 +0800

    AccessorFactory

---
+++ IBatisNetSelf.Common/Utilities/Objects/Members/ReflectionFieldSetAccessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBatisNetSelf.Common.Utilities.Objects.Members
+{
+    /// <summary>
+    /// The <see cref="ReflectionFieldSetAccessor"/> class provides an reflection set access   
+    /// to a field of a specified target class.
+    /// </summary>
+    public sealed class ReflectionFieldSetAccessor : ISetAccessor
+    {
+        private FieldInfo fieldInfo = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReflectionFieldSetAccessor"/> class.
+        /// </summary>
+        /// <param name="targetType">Type of the target.</param>
+        /// <param name="fieldName">Name of the field.</param>
+        public ReflectionFieldSetAccessor(Type targetType, string fieldName)
+        {
+            ReflectionInfo reflectionCache = ReflectionInfo.GetInstance(targetType);
+            fieldInfo = (FieldInfo)reflectionCache.GetGetter(fieldName);
+        }
+
+        #region ISetAccessor Members
+
+        /// <summary>
+        /// Gets the member name.
+        /// </summary>
+        public string Name=>fieldInfo.Name;
+
+        /// <summary>
+        /// Gets the type of this member, such as field, property.
+        /// </summary>
+        public Type MemberType=>fieldInfo.FieldType;
+
+        /// <summary>
+        /// Sets the value for the field of the specified target.
+        /// </summary>
+        /// <param name="target">Object to set the property on.</param>
+        /// <param name="value">Property value.</param>
+        public void Set(object target, object value)
+        {
+            fieldInfo.SetValue(target, value);
+        }
+
+        #endregion
+    }
+}
---
+++ IBatisNetSelf.Common/Utilities/Objects/Members/ReflectionPropertySetAccessor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBatisNetSelf.Common.Utilities.Objects.Members
+{
+    /// <summary>
+    /// The <see cref="ReflectionPropertySetAccessor"/> class provides an reflection set access   
+    /// to a property of a specified target class.
+    /// </summary>
+    public sealed class ReflectionPropertySetAccessor : ISetAccessor
+    {
+        private PropertyInfo propertyInfo = null;
+        private string propertyName = string.Empty;
+        private Type targetType = null;
+
+        #region IAccessor Members
+
+        /// <summary>
+        /// Gets the property name.
+        /// </summary>
+        public string Name=> this.propertyName;
+
+        /// <summary>
+        /// Gets the type of this property.
+        /// </summary>
+        public Type MemberType=>this.propertyInfo.PropertyType;
+
+        #endregion
+
+        #region ISet Members
+
+        /// <summary>
+        /// Sets the value for the property of the specified target.
+        /// </summary>
+        /// <param name="aTarget">Object to set the property on.</param>
+        /// <param name="aValue">Property value.</param>
+        public void Set(object aTarget, object aValue)
+        {
+            if (this.propertyInfo.CanWrite)
+            {
+                this.propertyInfo.SetValue(aTarget, aValue, null);
+            }
+            else
+            {
+                throw new NotSupportedException(
+                    $"Property \"{propertyName}\" on type {targetType} doesn't have a set method.");
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReflectionPropertySetAccessor"/> class.
+        /// </summary>
+        /// <param name="aTargetType">Type of the target.</param>
+        /// <param name="aPropertyName">Name of the property.</param>
+		public ReflectionPropertySetAccessor(Type aTargetType, string aPropertyName)
+        {
+            ReflectionInfo _reflectionCache = ReflectionInfo.GetInstance(aTargetType);
+            this.propertyInfo = (PropertyInfo)_reflectionCache.GetSetter(aPropertyName);
+
+            this.targetType = aTargetType;
+            this.propertyName = aPropertyName;
+        }
+
+    }
+}
