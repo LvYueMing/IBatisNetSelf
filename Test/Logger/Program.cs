16debd27bfa2ead1d6b0b2c54b3e074a93325a06
LvYueMing <350087689@qq.com>
2024-08-21 11:37:26 +0800

    调整logger测试程序

---
+++ Test/Logger/Program.cs
@@ -0,0 +1,16 @@
+﻿using IBatisNetSelf.Common.Logging;
+
+namespace TestLog
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            logger.Info("Test");
+
+            Console.ReadLine();
+        }
+    }
+}
